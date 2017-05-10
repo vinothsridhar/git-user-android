5712a9742ba642cbc5d5762bc1efb9e5708c866d
sridhar <sridhar@passportparking.com>
2017-05-10 15:48:31 +0530

    more abstraction

---
+++ app/src/main/java/sri/git/user/view/adapter/GitUserRepoAdapter.java
@@ -0,0 +1,8 @@
+package sri.git.user.view.adapter;
+
+/**
+ * Created by sridhar on 10/5/17.
+ */
+
+public class GitUserRepoAdapter {
+}
---
+++ app/src/main/java/sri/git/user/viewmodel/GitUserRepoItemViewModel.java
@@ -0,0 +1,8 @@
+package sri.git.user.viewmodel;
+
+/**
+ * Created by sridhar on 10/5/17.
+ */
+
+public class GitUserRepoItemViewModel {
+}
